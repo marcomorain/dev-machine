ac43c6ec5a1388bb4ac24fc720ec3efcdd63fc0f
Marc O'Morain <marc@circleci.com>
2015-06-17 16:59:18 +0100

    Fix rabbit

--- Vagrantfile
+++ Vagrantfile
@@ -6,7 +6,8 @@ Vagrant.configure(2) do |config|
   config.vm.network "forwarded_port", guest: 6005, host: 6005
   config.vm.synced_folder "..", "/vagrant_dev"
   config.vm.provider "virtualbox" do |vb|
-     vb.memory = "8096"
-   end
+    vb.memory = "8096"
+    vb.customize ["guestproperty", "set", :id, "/VirtualBox/GuestAdd/VBoxService/--timesync-set-threshold", 10000]
+  end
   config.vm.provision :shell, path: "bootstrap.sh"
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -6,54 +6,40 @@ POSTGRES_KEY_URL="https://www.postgresql.org/media/keys/ACCC4CF8.asc"
 LEIN_URL="https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein"
 LEIN_BIN=/usr/local/bin/lein
 
-echo foo one
-
 # Add keys
 curl --silent $POSTGRES_KEY_URL | apt-key add -
-echo foo two
-
-#curl --silent $RABBIT_KEY_URL   | apt-key add -
-echo foo three
 
 gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 7F0CEB10
 gpg --export --armor 7F0CEB10  | apt-key add -
 
-echo foo a
 apt-get update #> /dev/null
 apt-get install -y python-software-properties
 
-echo foo b
 # Add repositories
 echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 add-apt-repository "deb http://ftp.heanet.ie/pub/ubuntu/ trusty  main"
 add-apt-repository "deb mirror://mirrors.ubuntu.com/mirrors.txt trusty main restricted universe multiverse"
 add-apt-repository "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main"
-add-apt-repository "deb http://www.rabbitmq.com/debian/ testing main"
 add-apt-repository ppa:webupd8team/java
 
-echo foo c
 # Accept Java TOS
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
 
-echo foo d
 apt-get update #> /dev/null
-
-echo foo dd
 apt-get install -y \
   oracle-java8-installer \
   git \
   postgresql-9.4 \
   postgresql-contrib-9.4 \
   mongodb-org \
-  redis-server \
-  rabbitmq-server
+  redis-server #\
 
-echo foo e
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.deb.sh | bash
+
 apt-get autoremove -y
 
-echo foo f
 # Add leinigen
 curl --silent --output $LEIN_BIN $LEIN_URL
 chmod 755 $LEIN_BIN
 
-echo foo g
+echo 'Bootstrap complete'
